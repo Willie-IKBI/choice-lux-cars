1a64af0077a6eaae5f16da9b1ffa07b1fd00e50a
Willie Erasmus <erasm.w@ikbi.co.za>
2025-08-24 22:26:34 +0200

    Fix missing go_router import in user profile screen back button

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:choice_lux_cars/app/theme.dart';
 import 'package:choice_lux_cars/features/auth/providers/auth_provider.dart';
