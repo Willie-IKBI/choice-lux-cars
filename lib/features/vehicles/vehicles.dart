b02241eeeb11322d311c8977ec35d9cf1fedd7bb
Willie Erasmus <willie@ikbi.co.za>
2025-12-28 15:38:27 +0200

    chore(vehicles): add feature barrel export

---
+++ lib/features/vehicles/vehicles.dart
@@ -0,0 +1,6 @@
+// Public exports for the Vehicles feature.
+export 'vehicles_screen.dart';
+export 'vehicle_editor_screen.dart';
+export 'providers/vehicles_provider.dart';
+export 'models/vehicle.dart';
+export 'data/vehicles_repository.dart';
