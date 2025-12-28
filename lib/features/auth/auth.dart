6a05017f29ca112aceabca7664471264c7ca0f4b
Willie Erasmus <willie@ikbi.co.za>
2025-12-28 16:49:07 +0200

    chore(auth): add feature barrel export

---
+++ lib/features/auth/auth.dart
@@ -0,0 +1,10 @@
+// Public exports for the Auth feature.
+
+export 'pending_approval_screen.dart';
+
+export 'login/login_screen.dart';
+export 'signup/signup_screen.dart';
+export 'forgot_password/forgot_password_screen.dart';
+export 'reset_password/reset_password_screen.dart';
+
+export 'providers/auth_provider.dart';
