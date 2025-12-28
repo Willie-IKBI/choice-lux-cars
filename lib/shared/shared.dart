6d0e03a5697fa5ab47acac220afe5fd6b418b295
Willie Erasmus <willie@ikbi.co.za>
2025-12-28 15:04:27 +0200

    chore(shared): add shared barrel export

---
+++ lib/shared/shared.dart
@@ -0,0 +1,13 @@
+library shared;
+
+// Widgets
+export 'widgets/dashboard_card.dart';
+export 'widgets/luxury_app_bar.dart';
+export 'widgets/luxury_drawer.dart';
+
+// Utils
+export 'utils/sa_time_utils.dart';
+
+// Mixins
+export 'mixins/gps_capture_mixin.dart';
+export 'mixins/image_picker_mixin.dart';
