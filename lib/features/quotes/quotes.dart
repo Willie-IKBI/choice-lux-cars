c07c5886f9f7d5c3ec89da267a003bd0db70c5a4
Willie Erasmus <willie@ikbi.co.za>
2025-12-28 16:14:24 +0200

    chore(quotes): add feature barrel export

---
+++ lib/features/quotes/quotes.dart
@@ -0,0 +1,19 @@
+// Public exports for the Quotes feature.
+// Import this file instead of deep-path imports when possible.
+
+export 'quotes_screen.dart';
+
+export 'screens/create_quote_screen.dart';
+export 'screens/quote_details_screen.dart';
+export 'screens/quote_transport_details_screen.dart';
+
+export 'models/quote.dart';
+export 'models/quote_transport_detail.dart';
+
+export 'providers/quotes_provider.dart';
+
+export 'data/quotes_repository.dart';
+
+export 'services/quote_pdf_service.dart';
+
+export 'widgets/quote_card.dart';
