d106475aa37d5bf1df24068d7a5968609d23468f
Willie Erasmus <willie@ikbi.co.za>
2025-12-28 17:01:19 +0200

    chore(clients): add feature barrel export

---
+++ lib/features/clients/clients.dart
@@ -0,0 +1,29 @@
+// Public exports for the Clients feature.
+
+export 'clients_screen.dart';
+export 'inactive_clients_screen.dart';
+
+// Screens
+export 'screens/add_edit_client_screen.dart';
+export 'screens/client_detail_screen.dart';
+export 'screens/add_edit_agent_screen.dart';
+export 'screens/edit_client_screen.dart';
+
+// Models
+export 'models/client.dart';
+export 'models/client_branch.dart';
+export 'models/agent.dart';
+
+// Providers
+export 'providers/clients_provider.dart';
+export 'providers/agents_provider.dart';
+export 'providers/client_stats_provider.dart';
+
+// Data / Services
+export 'data/clients_repository.dart';
+export 'data/agents_repository.dart';
+
+// Widgets
+export 'widgets/client_card.dart';
+export 'widgets/agent_card.dart';
+export 'widgets/branch_management_modal.dart';
